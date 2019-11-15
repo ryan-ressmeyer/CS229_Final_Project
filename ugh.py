bb88751c3c7a3e160feebd15596fb99efa15718d
Sam Masling <sam.masling@gmail.com>
2019-11-15 15:29:50 -0800

    changes

--- ugh.py
+++ ugh.py
@@ -1,11 +1,10 @@
-import numpy as np
-import pandas as pd
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 
 
-aaa = np.array([1,3,3])
-bbb = np.array([6,6,6])
-df = pd.DataFrame(np.array([aaa,bbb]).T, columns=["A", "B"])
+for i in stopwords.words('english'):
+    print(i)
 
-print(df)
 
 
